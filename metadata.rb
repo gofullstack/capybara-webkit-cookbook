a90e03572a4031308db9b02a01fe3516f40bca2b
Nathan L Smith <nlloyds@gmail.com>
2012-06-17 20:32:14 -0500

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Cramer Development, Inc."
+maintainer_email "sysadmin@cramerdev.com"
+license          "Apache 2.0"
+description      "Installs/Configures capybara-webkit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+supports         "ubuntu"
